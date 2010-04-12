bf4b822df27e8a90d3214dee3b7e145788b3539c
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2010-04-13 01:30:47 +0200

    Update setup to be PyPI-friendly.

--- setup.py
+++ setup.py
@@ -18,9 +18,20 @@
 # along with Radicale.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Radicale setup file.
+Radicale CalDAV server
+======================
 
-To install, type ``python setup.py install`` as superuser.
+The Radicale Project is a CalDAV calendar server.  It aims to be a light
+solution, easy to use, easy to install, easy to configure.  As a consequence,
+it requires few software dependances and is pre-configured to work
+out-of-the-box.
+
+The Radicale Project runs on most of the UNIX-like platforms (Linux, *BSD,
+MacOS X) and Windows.  It is known to work with Lightning and Sunbird 0.9+. It
+is free and open-source software, released under GPL version 3.
+
+For further information, please visit the `Radicale Website
+<http://www.radicale.org/>`_.
 
 """
 
@@ -78,12 +89,30 @@ class Clean(Command):
 setup(
     name="Radicale",
     version="0.2",
-    description="Radicale CalDAV Server",
+    description="CalDAV Server",
+    long_description=__doc__,
     author="Guillaume Ayoub",
     author_email="guillaume.ayoub@kozea.fr",
     url="http://www.radicale.org/",
     license="GNU GPL v3",
+    platforms="Any",
     packages=["radicale", "radicale.acl"],
+    provides=["radicale"],
     scripts=["radicale.py"],
-    cmdclass={'clean': Clean,
-              "build_scripts": BuildScripts})
+    cmdclass={"clean": Clean,
+              "build_scripts": BuildScripts},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Topic :: Office/Business :: Groupware"])
