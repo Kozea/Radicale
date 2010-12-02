77ff57eb725e660960c221b0cdec1f79752665a9
System User <corentin@deb-co.alpes-ressources.dom>
2010-12-02 16:42:16 +0100

    Add class for logging in log file

---
+++ radicale/log.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import logging
+
+from radicale import config
+
+LEVELS = {	'debug': logging.DEBUG,
+			'info': logging.INFO,
+			'warning': logging.WARNING,
+			'error': logging.ERROR,
+			'critical': logging.CRITICAL}
+
+level=LEVELS.get(config.get("logging", "level"), logging.NOTSET)
+
+logger=logging.getLogger("radicale")
+logger.setLevel(level=level)
+
+handler=logging.FileHandler(config.get("logging", "file"))
+handler.setLevel(level=level)
+		
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+		
+handler.setFormatter(formatter)
+		
+logger.addHandler(handler)
+
+sys.modules[__name__] = logger
