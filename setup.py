9f19481a69e510a36e1a4d457b5cfe3a9fbc2a26
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2011-04-02 12:06:08 +0200

    Add support for Python 3.2

--- setup.py
+++ setup.py
@@ -91,4 +91,5 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.0",
         "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
         "Topic :: Office/Business :: Groupware"])
