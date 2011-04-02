9f723e5c7b62f6a87efd5aa2c109c8f4e1437a21
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2011-04-02 12:03:33 +0200

    Update package description

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ it requires few software dependances and is pre-configured to work
 out-of-the-box.
 
 The Radicale Project runs on most of the UNIX-like platforms (Linux, BSD,
-MacOS X) and Windows.  It is known to work with Evolution 2.30+, Lightning 0.9+
-and Sunbird 0.9+. It is free and open-source software, released under GPL
+MacOS X) and Windows.  It is known to work with Evolution, Lightning, iPhone
+and Android clients. It is free and open-source software, released under GPL
 version 3.
 
 For further information, please visit the `Radicale Website
