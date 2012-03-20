7faec9abc9cf55a0288b89f550d23197b392e938
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2012-03-20 18:41:14 +0100

    Update the software description

--- setup.py
+++ setup.py
@@ -18,13 +18,13 @@
 # along with Radicale.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Radicale CalDAV server
-======================
+Radicale CalDAV and CardDAV server
+==================================
 
-The Radicale Project is a CalDAV calendar server.  It aims to be a light
-solution, easy to use, easy to install, easy to configure.  As a consequence,
-it requires few software dependances and is pre-configured to work
-out-of-the-box.
+The Radicale Project is a CalDAV (calendar) and CardDAV (contact) server.  It
+aims to be a light solution, easy to use, easy to install, easy to configure.
+As a consequence, it requires few software dependances and is pre-configured to
+work out-of-the-box.
 
 The Radicale Project runs on most of the UNIX-like platforms (Linux, BSD,
 MacOS X) and Windows.  It is known to work with Evolution, Lightning, iPhone
@@ -46,7 +46,7 @@ import radicale
 setup(
     name="Radicale",
     version=radicale.VERSION,
-    description="CalDAV Server",
+    description="CalDAV and CardDAV Server",
     long_description=__doc__,
     author="Guillaume Ayoub",
     author_email="guillaume.ayoub@kozea.fr",
