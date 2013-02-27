b6b48b5af7885183be9ac631d3b39ec178111cb2
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2013-02-27 10:36:54 +0100

    Typo

--- radicale/xmlutils.py
+++ radicale/xmlutils.py
@@ -197,16 +197,14 @@ def propfind(path, xml_request, collections, user=None):
     # Reading request
     if xml_request:
         root = ET.fromstring(xml_request.encode("utf8"))
-
-        prop_element = root.find(_tag("D", "prop"))
-        props = [prop.tag for prop in prop_element]
+        props = [prop.tag for prop in root.find(_tag("D", "prop"))]
     else:
-        props = [ _tag("D", "getcontenttype"),
-            _tag("D", "resourcetype"),
-            _tag("D", "displayname"),
-            _tag("D", "owner"),
-            _tag("D", "getetag"),
-            _tag("CS", "getctag") ]
+        props = [_tag("D", "getcontenttype"),
+                 _tag("D", "resourcetype"),
+                 _tag("D", "displayname"),
+                 _tag("D", "owner"),
+                 _tag("D", "getetag"),
+                 _tag("CS", "getctag")]
 
     # Writing answer
     multistatus = ET.Element(_tag("D", "multistatus"))
