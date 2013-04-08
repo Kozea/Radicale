7a96578ca427342b12579f095fe84afed2d9371b
Jonathan Dupart <jonathan@dupart.org>
2013-04-09 00:42:25 +0200

    Typo: missing comma
    sync-collection and expand-property where returned concatenated in
    PROPFIND answers.

--- radicale/xmlutils.py
+++ radicale/xmlutils.py
@@ -278,7 +278,7 @@ def _propfind_response(path, item, props, user):
             element.append(privilege)
         elif tag == _tag("D", "supported-report-set"):
             for report_name in (
-                    "principal-property-search", "sync-collection"
+                    "principal-property-search", "sync-collection",
                     "expand-property", "principal-search-property-set"):
                 supported = ET.Element(_tag("D", "supported-report"))
                 report_tag = ET.Element(_tag("D", "report"))
