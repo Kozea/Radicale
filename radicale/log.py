f09b070e40ae1f0fd8223074e62881dd8825bc33
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2013-08-27 15:19:15 +0200

    Fix stupid bug in logs

--- radicale/log.py
+++ radicale/log.py
@@ -55,5 +55,5 @@ def start():
         if debug:
             LOGGER.setLevel(logging.DEBUG)
             LOGGER.debug(
-                "Logging configuration file '%s' not found, using stdout.",
+                "Logging configuration file '%s' not found, using stdout." %
                 filename)
