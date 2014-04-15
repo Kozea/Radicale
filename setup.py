58370ef6414767376ec1f6b48e6392b4578110a4
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2014-04-15 18:24:44 +0200

    Add python 3.4 in setup.py

--- setup.py
+++ setup.py
@@ -73,4 +73,5 @@ setup(
         "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Office/Business :: Groupware"])
