a65f96fe41007cacac16b1669bb4a75dc933096d
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2014-07-28 11:28:12 +0200

    Fix syntax error

--- radicale/__init__.py
+++ radicale/__init__.py
@@ -340,7 +340,7 @@ class Application(object):
                user):
         """Manage DELETE request."""
         if not len(write_collections):
-            return return NOT_ALLOWED
+            return NOT_ALLOWED
 
         collection = write_collections[0]
 
