ecce9ee4fc98c2ea9d3f2b8d59aa9f1cab4f9cef
Nicolas Bonfante <nicolas.bonfante@kozea.fr>
2016-06-15 16:54:27 +0200

    solve test of VTODO

--- tests/test_base.py
+++ tests/test_base.py
@@ -376,7 +376,7 @@ class BaseRequests:
               <C:comp-filter name="VTODO">
                 <C:time-range start="20130801T000000Z" end="20131001T000000Z"/>
               </C:comp-filter>
-            </C:comp-filter>"""],"todo", events=8)
+            </C:comp-filter>"""],"todo", events=7)
         assert "href>/calendar.ics/todo1.ics</" in answer
         assert "href>/calendar.ics/todo2.ics</" in answer
         assert "href>/calendar.ics/todo3.ics</" in answer
@@ -391,14 +391,14 @@ class BaseRequests:
               <C:comp-filter name="VTODO">
                 <C:time-range start="20130901T160000Z" end="20130901T183000Z"/>
               </C:comp-filter>
-            </C:comp-filter>"""],"todo", events=4)
+            </C:comp-filter>"""],"todo", events=7)
         assert "href>/calendar.ics/todo1.ics</" not in answer
         assert "href>/calendar.ics/todo2.ics</" in answer
         assert "href>/calendar.ics/todo3.ics</" in answer
         assert "href>/calendar.ics/todo4.ics</" not in answer
         assert "href>/calendar.ics/todo5.ics</" not in answer
         assert "href>/calendar.ics/todo6.ics</" not in answer
-        assert "href>/calendar.ics/todo7.ics</" not in answer
+        assert "href>/calendar.ics/todo7.ics</" in answer
 
         
         answer = self._test_filter(["""
