a5e3ffa4270de4ac3d96929a152695012a291715
Peter Bieringer <pb@bieringer.de>
2016-09-18 19:30:51 +0200

    realign change

--- radicale/storage.py
+++ radicale/storage.py
@@ -742,14 +742,7 @@ class Collection(BaseCollection):
                         break
             return collection.serialize()
         elif self.get_meta("tag") == "VADDRESSBOOK":
-            items_ok = []
-            for item in items:
-                try:
-                    item.serialize()
-                    items_ok.append(item)
-                except:
-                    self.logger.exception("VCARD broken (skip): %s", item)
-            return "".join([item.serialize() for item in items_ok])
+            return "".join([item.serialize() for item in items])
         return ""
 
     _lock = threading.Lock()
