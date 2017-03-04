9900251b8b9770e15036d048d4b8e98915300f39
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2017-03-04 14:37:58 +0100

    Add Python 3.6 classifier in setup.py

--- setup.py
+++ setup.py
@@ -85,4 +85,5 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Office/Business :: Groupware"])
