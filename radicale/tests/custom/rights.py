3e715a9aff39ad6c1c95b089cf47af18ddeab289
Unrud <unrud@openaliasbox.org>
2017-05-31 00:36:20 +0200

    Test rights management

---
+++ radicale/tests/custom/rights.py
@@ -0,0 +1,27 @@
+# This file is part of Radicale Server - Calendar Server
+# Copyright (C) 2017 Unrud <unrud@openaliasbox.org>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Radicale.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Custom rights management.
+
+"""
+
+from radicale import rights
+
+
+class Rights(rights.BaseRights):
+    def authorized(self, user, path, permission):
+        return path.strip("/") in ("tmp", "other")
