eae1577525727554835aa3d6caf8f5c8fc89a599
Frank Sachsenheim <funkyfuture@riseup.net>
2018-01-14 19:28:16 +0100

    Docker: Some sanitation in the entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,9 +3,15 @@
 RADICALE_UID="${RADICALE_UID:-1000}"
 RADICALE_GID="${RADICALE_GID:-1000}"
 
-addgroup   -g ${RADICALE_GID} radicale
-adduser -D -s /bin/false -u ${RADICALE_GID} -G radicale radicale
+getent group  radicale &> /dev/null || addgroup -g ${RADICALE_GID} radicale
+getent passwd radicale &> /dev/null || adduser -D -H -s /bin/false -u ${RADICALE_GID} -G radicale radicale
 
-chown -R radicale:radicale "$(dirname "${RADICALE_CONFIG:-/etc/radicale/radicale.config}")" /var/lib/radicale
+if [ -n "${RADICALE_CONFIG}" ]; then
+    chown -R radicale:radicale $(dirname $RADICALE_CONFIG) /var/lib/radicale
+    CONF_OPT="--config '${RADICALE_CONFIG}'"
+else
+    chown -R radicale:radicale /etc/radicale /var/lib/radicale
+    CONF_OPT=""
+fi
 
-exec su - radicale -s /bin/sh -c "exec radicale --hosts 0.0.0.0:5232 ${RADICALE_CONFIG:+--config='${RADICALE_CONFIG}'} $*"
+exec su -s /bin/sh -c "exec radicale --hosts 0.0.0.0:5232 ${CONF_OPT} $*" radicale
