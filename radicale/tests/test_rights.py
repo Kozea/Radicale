f6081bb07e10f42e607b04d0f67f1e98c1def9f9
Unrud <unrud@outlook.com>
2018-04-20 14:57:29 +0200

    Update email address

--- radicale/tests/test_rights.py
+++ radicale/tests/test_rights.py
@@ -1,5 +1,5 @@
 # This file is part of Radicale Server - Calendar Server
-# Copyright (C) 2017 Unrud <unrud@openaliasbox.org>
+# Copyright (C) 2017 Unrud <unrud@outlook.com>
 #
 # This library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- radicale/web/fn.js
+++ radicale/web/fn.js
@@ -1,6 +1,6 @@
 /**
  * This file is part of Radicale Server - Calendar Server
- * Copyright (C) 2017 Unrud <unrud@openaliasbox.org>
+ * Copyright (C) 2017 Unrud <unrud@outlook.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
