e62aa4f767e5f6164f96be2b1164fbb04666f252
Unrud <unrud@outlook.com>
2018-08-14 18:46:38 +0200

    require vobject>=0.9.6

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     packages=["radicale"],
     package_data={"radicale": WEB_FILES},
     entry_points={"console_scripts": ["radicale = radicale.__main__:run"]},
-    install_requires=["vobject==0.9.5", "python-dateutil==2.6.1"],
+    install_requires=["vobject>=0.9.6"],
     setup_requires=pytest_runner,
     tests_require=tests_require,
     extras_require={
