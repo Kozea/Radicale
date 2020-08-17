1289003da160cf55378ccf3b1f518e78c9d937bb
Tuna Celik <tuna@jakpark.com>
2020-08-17 02:37:21 +0200

    Refactoring

--- radicale/hook/__init__.py
+++ radicale/hook/__init__.py
@@ -40,4 +40,9 @@ class HookNotificationItem:
         self.content = content
 
     def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+        return json.dumps(
+            self,
+            default=lambda o: o.__dict__,
+            sort_keys=True,
+            indent=4
+        )
