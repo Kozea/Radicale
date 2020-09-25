dd30aea7a572f1e580b8eea2e22fdc01a638d6a2
Unrud <unrud@outlook.com>
2020-09-25 23:36:15 +0200

    Add Python 3.9 classifier

--- setup.py
+++ setup.py
@@ -90,4 +90,5 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Office/Business :: Groupware"])
