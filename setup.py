ee5489dcd6d8c84365d4dd78426d0f057c932302
Unrud <unrud@outlook.com>
2020-09-26 03:20:56 +0200

    Add Python implementation classifier

--- setup.py
+++ setup.py
@@ -91,4 +91,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Office/Business :: Groupware"])
