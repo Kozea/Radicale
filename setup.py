bbaf0ebd8cd85efe6bca2ce1a5b648c908c89d43
Unrud <unrud@outlook.com>
2021-12-08 21:45:42 +0100

    Change name in file header

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# This file is part of Radicale Server - Calendar Server
+# This file is part of Radicale - CalDAV and CardDAV server
 # Copyright © 2009-2017 Guillaume Ayoub
 # Copyright © 2017-2018 Unrud <unrud@outlook.com>
 #
