33fcda7c32f9ab1d3e78c96ca38759bcddc55eaf
Unrud <unrud@outlook.com>
2022-01-18 18:20:16 +0100

    Extract httputils.serve_folder

--- radicale/web/internal.py
+++ radicale/web/internal.py
@@ -25,32 +25,10 @@ Features:
 
 """
 
-
-import os
-import time
-from http import client
-from typing import Mapping
-
 import pkg_resources
 
-from radicale import config, httputils, pathutils, types, web
-from radicale.log import logger
-
-MIMETYPES: Mapping[str, str] = {
-    ".css": "text/css",
-    ".eot": "application/vnd.ms-fontobject",
-    ".gif": "image/gif",
-    ".html": "text/html",
-    ".js": "application/javascript",
-    ".manifest": "text/cache-manifest",
-    ".png": "image/png",
-    ".svg": "image/svg+xml",
-    ".ttf": "application/font-sfnt",
-    ".txt": "text/plain",
-    ".woff": "application/font-woff",
-    ".woff2": "font/woff2",
-    ".xml": "text/xml"}
-FALLBACK_MIMETYPE: str = "application/octet-stream"
+from radicale import config, httputils, types, web
+from radicale.httputils import FALLBACK_MIMETYPE, MIMETYPES  # noqa:F401
 
 
 class Web(web.BaseWeb):
@@ -59,34 +37,9 @@ class Web(web.BaseWeb):
 
     def __init__(self, configuration: config.Configuration) -> None:
         super().__init__(configuration)
-        self.folder = pkg_resources.resource_filename(__name__,
-                                                      "internal_data")
+        self.folder = pkg_resources.resource_filename(
+            __name__, "internal_data")
 
     def get(self, environ: types.WSGIEnviron, base_prefix: str, path: str,
             user: str) -> types.WSGIResponse:
-        assert path == "/.web" or path.startswith("/.web/")
-        assert pathutils.sanitize_path(path) == path
-        try:
-            filesystem_path = pathutils.path_to_filesystem(
-                self.folder, path[len("/.web"):].strip("/"))
-        except ValueError as e:
-            logger.debug("Web content with unsafe path %r requested: %s",
-                         path, e, exc_info=True)
-            return httputils.NOT_FOUND
-        if os.path.isdir(filesystem_path) and not path.endswith("/"):
-            return httputils.redirect(base_prefix + path + "/")
-        if os.path.isdir(filesystem_path):
-            filesystem_path = os.path.join(filesystem_path, "index.html")
-        if not os.path.isfile(filesystem_path):
-            return httputils.NOT_FOUND
-        content_type = MIMETYPES.get(
-            os.path.splitext(filesystem_path)[1].lower(), FALLBACK_MIMETYPE)
-        with open(filesystem_path, "rb") as f:
-            answer = f.read()
-            last_modified = time.strftime(
-                "%a, %d %b %Y %H:%M:%S GMT",
-                time.gmtime(os.fstat(f.fileno()).st_mtime))
-        headers = {
-            "Content-Type": content_type,
-            "Last-Modified": last_modified}
-        return client.OK, headers, answer
+        return httputils.serve_folder(self.folder, base_prefix, path)
