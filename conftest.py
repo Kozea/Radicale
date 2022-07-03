1dda835084624a8ddc98cafcc6fa3aaa16be318e
Unrud <unrud@outlook.com>
2022-07-03 10:14:38 +0200

    Use tox for tests

--- conftest.py
+++
@@ -1,9 +0,0 @@
-import sys
-from importlib.util import find_spec
-
-
-def pytest_addoption(parser, pluginmanager):
-    # Ignore the "--mypy" argument if pytest-mypy is not installed and
-    # the implementation is not cpython
-    if sys.implementation.name != 'cpython' and not find_spec("pytest_mypy"):
-        parser.addoption("--mypy", action="store_true")
