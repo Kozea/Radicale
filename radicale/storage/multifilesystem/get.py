e02a31af89429f858a0c7880c15a93020b75c80d
Peter Bieringer <pb@bieringer.de>
2024-06-09 14:35:14 +0200

    fix flake8 error report

--- radicale/storage/multifilesystem/get.py
+++ radicale/storage/multifilesystem/get.py
@@ -107,7 +107,7 @@ class CollectionPartGet(CollectionPartCache, CollectionPartLock,
                             return
                         else:
                             raise RuntimeError("Failed to load item %r in %r: %s" %
-                                           (href, self.path, e)) from e
+                                               (href, self.path, e)) from e
                     # Clean cache entries once after the data in the file
                     # system was edited externally.
                     if not self._item_cache_cleaned:
