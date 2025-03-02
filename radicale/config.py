68f0eafe7db4a00153c9f3457433e5c088d79467
Peter Bieringer <pb@bieringer.de>
2025-03-02 09:02:10 +0100

    script_name: add config option, fixes https://github.com/Kozea/Radicale/issues/1275

--- radicale/config.py
+++ radicale/config.py
@@ -187,6 +187,10 @@ DEFAULT_CONFIG_SCHEMA: types.CONFIG_SCHEMA = OrderedDict([
             "help": "set CA certificate for validating clients",
             "aliases": ("--certificate-authority",),
             "type": filepath}),
+        ("script_name", {
+            "value": "",
+            "help": "script name to strip from URI if called by reverse proxy (default taken from HTTP_X_SCRIPT_NAME or SCRIPT_NAME)",
+            "type": str}),
         ("_internal_server", {
             "value": "False",
             "help": "the internal server is used",
