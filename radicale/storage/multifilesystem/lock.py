af09d532c36a6ebb8e79d3a1e8befb61fcb7de35
Peter Bieringer <pb@bieringer.de>
2025-03-27 07:57:28 +0100

    catch unsupported placeholder

--- radicale/storage/multifilesystem/lock.py
+++ radicale/storage/multifilesystem/lock.py
@@ -74,8 +74,13 @@ class StoragePartLock(StorageBase):
                 else:
                     # Process group is also used to identify child processes
                     preexec_fn = os.setpgrp
-                command = self._hook % {
-                    "user": shlex.quote(user or "Anonymous")}
+                try:
+                    command = self._hook % {
+                        "user": shlex.quote(user or "Anonymous")}
+                except KeyError as e:
+                    logger.error("Storage hook contains not supported placeholder %s (skip execution of: %r)" % (e, self._hook))
+                    return
+
                 logger.debug("Executing storage hook: '%s'" % command)
                 try:
                     p = subprocess.Popen(
