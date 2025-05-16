9e1cac98dc350720aaf07b49d4491c66c0ac5f3e
Peter Bieringer <pb@bieringer.de>
2025-05-16 06:31:39 +0200

    make isort happy

--- radicale/tests/test_base.py
+++ radicale/tests/test_base.py
@@ -21,12 +21,12 @@ Radicale tests with simple requests.
 
 """
 
+import logging
 import os
 import posixpath
 from typing import Any, Callable, ClassVar, Iterable, List, Optional, Tuple
 
 import defusedxml.ElementTree as DefusedET
-import logging
 import vobject
 
 from radicale import storage, xmlutils
