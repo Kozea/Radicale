aaae83e588327be72da33e274204d4053ff8e3c4
Rémy Marquis <remy.marquis@heig-vd.ch>
2025-06-18 11:34:24 +0200

    Fix CORS with custom headers

--- radicale/privacy/http.py
+++ radicale/privacy/http.py
@@ -31,7 +31,7 @@ class PrivacyHTTP(ApplicationBase):
     CORS_HEADERS = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
         "Access-Control-Expose-Headers": "Authorization",
         "Access-Control-Max-Age": "86400",
     }
