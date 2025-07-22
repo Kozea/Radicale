fe175df9a2fa530c8692d31326159e4cf6ed59c2
Peter Bieringer <pb@bieringer.de>
2025-07-22 20:58:10 +0200

    add missing copyright header

--- radicale/hook/email/__init__.py
+++ radicale/hook/email/__init__.py
@@ -1,6 +1,20 @@
 # This file is related to Radicale - CalDAV and CardDAV server
 # for email notifications
 # Copyright © 2025-2025 Nate Harris
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Radicale.  If not, see <http://www.gnu.org/licenses/>.
+
 import enum
 import re
 import smtplib
