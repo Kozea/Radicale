0e44b48327e9c6b5965a26919d628995bddf4120
Peter Bieringer <pb@bieringer.de>
2025-07-22 20:58:59 +0200

    add missing copyright header

--- radicale/hook/rabbitmq/__init__.py
+++ radicale/hook/rabbitmq/__init__.py
@@ -1,3 +1,20 @@
+# This file is part of Radicale - CalDAV and CardDAV server
+# Copyright © 2020-2024 Tuna Celik <tuna@jakpark.com>
+# Copyright © 2025-2025 Peter Bieringer <pb@bieringer.de>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Radicale.  If not, see <http://www.gnu.org/licenses/>.
+
 import pika
 from pika.exceptions import ChannelWrongStateError, StreamLostError
 
