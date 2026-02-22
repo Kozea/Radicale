8d0447f0c7c49db37d0e27ff122bdefe8e6124cb
Peter Bieringer <pb@bieringer.de>
2026-02-22 12:30:39 +0100

    extend copyright

--- radicale/app/delete.py
+++ radicale/app/delete.py
@@ -3,7 +3,7 @@
 # Copyright © 2008 Pascal Halter
 # Copyright © 2008-2017 Guillaume Ayoub
 # Copyright © 2017-2020 Unrud <unrud@outlook.com>
-# Copyright © 2024-2025 Peter Bieringer <pb@bieringer.de>
+# Copyright © 2024-2026 Peter Bieringer <pb@bieringer.de>
 #
 # This library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
