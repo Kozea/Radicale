2e1f54c7a871fb2e0e45d7ea17c072ae0581dbd9
Peter Bieringer <pb@bieringer.de>
2026-02-22 12:27:38 +0100

    extend copyright

--- radicale/app/base.py
+++ radicale/app/base.py
@@ -1,6 +1,6 @@
 # This file is part of Radicale - CalDAV and CardDAV server
 # Copyright © 2020 Unrud <unrud@outlook.com>
-# Copyright © 2024-2024 Peter Bieringer <pb@bieringer.de>
+# Copyright © 2024-2026 Peter Bieringer <pb@bieringer.de>
 #
 # This library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
