2379144772fd49d31d763ab704d1ac76111ee7f7
Peter Bieringer <pb@bieringer.de>
2026-02-22 12:38:49 +0100

    extend copyright

--- radicale/app/move.py
+++ radicale/app/move.py
@@ -3,7 +3,7 @@
 # Copyright © 2008 Pascal Halter
 # Copyright © 2008-2017 Guillaume Ayoub
 # Copyright © 2017-2023 Unrud <unrud@outlook.com>
-# Copyright © 2023-2025 Peter Bieringer <pb@bieringer.de>
+# Copyright © 2023-2026 Peter Bieringer <pb@bieringer.de>
 #
 # This library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
