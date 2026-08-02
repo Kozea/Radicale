e22570417455ba4d9ebc19c18417eb06e914359d
Peter Bieringer <pb@bieringer.de>
2026-08-02 16:22:08 +0200

    max_vevent_rrule_occurrence: add additional test cases

--- radicale/tests/test_base.py
+++ radicale/tests/test_base.py
@@ -382,12 +382,26 @@ permissions: RrWw""")
         event = get_file_content("event_full_day_rrule_until_50y.ics")
         self.put("/calendar.ics/event_full_day_rrule_until_50y.ics", event, check=400)
 
+    def test_add_event_with_rrule_until_50y_limit_20000(self) -> None:
+        """Test event with RRULE UNTIL=+50y and limit 20000."""
+        self.configure({"server": {"max_vevent_rrule_occurrence": 20000}})
+        self.mkcalendar("/calendar.ics/")
+        event = get_file_content("event_full_day_rrule_until_50y.ics")
+        self.put("/calendar.ics/event_full_day_rrule_until_50y.ics", event)
+
     def test_add_event_with_rrule_until_5000y_limit_100(self) -> None:
         """Test event with RRULE UNTIL=+5000y and limit 100."""
         self.configure({"server": {"max_vevent_rrule_occurrence": 100}})
         self.mkcalendar("/calendar.ics/")
         event = get_file_content("event_full_day_rrule_until_5000y.ics")
         self.put("/calendar.ics/event_full_day_rrule_until_5000y.ics", event, check=400)
+
+    def test_add_event_with_rrule_until_5000y_limit_2000000(self) -> None:
+        """Test event with RRULE UNTIL=+5000y and limit 2000000."""
+        self.configure({"server": {"max_vevent_rrule_occurrence": 2000000}})
+        self.mkcalendar("/calendar.ics/")
+        event = get_file_content("event_full_day_rrule_until_5000y.ics")
+        self.put("/calendar.ics/event_full_day_rrule_until_5000y.ics", event)
 
     def test_add_event_with_exdate_without_rrule(self) -> None:
         """Test event with EXDATE but not having RRULE."""
